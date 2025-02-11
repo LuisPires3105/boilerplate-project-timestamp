015ce07961417bb8b631fc89e37656103519bf07
Luis Pires <luispires310500@gmail.com>
2025-02-11 15:06:06 +0000

    Updated version of index.js

--- index.js
+++ index.js
@@ -24,7 +24,10 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+// your second API endpoint... 
+app.get("/api/other", function (req, res) {
+  res.json({greeting: 'other API'});
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
